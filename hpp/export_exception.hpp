36aee3fa3ed45cc717689f86bff8f93d24444fb0
philip82148 <92205270+philip82148@users.noreply.github.com>
2023-09-24 01:06:51 +0900

    don't use export_object() when export_exception()

--- hpp/export_exception.hpp
+++ hpp/export_exception.hpp
@@ -9,20 +9,40 @@
 
 #include <stdexcept>
 
-#include "./export_object.hpp"
 #include "./type_check.hpp"
 #include "./utility.hpp"
 
-CPP_DUMP_DEFINE_EXPORT_OBJECT(std::logic_error, what());
-CPP_DUMP_DEFINE_EXPORT_OBJECT(std::domain_error, what());
-CPP_DUMP_DEFINE_EXPORT_OBJECT(std::invalid_argument, what());
-CPP_DUMP_DEFINE_EXPORT_OBJECT(std::length_error, what());
-CPP_DUMP_DEFINE_EXPORT_OBJECT(std::out_of_range, what());
-CPP_DUMP_DEFINE_EXPORT_OBJECT(std::runtime_error, what());
-CPP_DUMP_DEFINE_EXPORT_OBJECT(std::range_error, what());
-CPP_DUMP_DEFINE_EXPORT_OBJECT(std::overflow_error, what());
-CPP_DUMP_DEFINE_EXPORT_OBJECT(std::underflow_error, what());
-CPP_DUMP_DEFINE_EXPORT_OBJECT(std::exception, what());
+#define _p_CPP_DUMP_DEFINE_EXPORT_EXCEPTION(TYPE)                                                  \
+  inline std::string export_exception(                                                             \
+      const TYPE &exception,                                                                       \
+      const std::string &indent,                                                                   \
+      size_t last_line_length,                                                                     \
+      size_t current_depth,                                                                        \
+      bool fail_on_newline                                                                         \
+  ) {                                                                                              \
+    size_t next_depth = current_depth + 1;                                                         \
+                                                                                                   \
+    std::string prefix = #TYPE "{ what()= ";                                                       \
+    std::string output =                                                                           \
+        prefix                                                                                     \
+        + export_var(                                                                              \
+            exception.what(), indent, last_line_length + prefix.length(), next_depth, true         \
+        )                                                                                          \
+        + " }";                                                                                    \
+                                                                                                   \
+    if (!has_newline(output) && output.length() <= max_line_width) return output;                  \
+                                                                                                   \
+    if (fail_on_newline) return "\n";                                                              \
+                                                                                                   \
+    std::string new_indent = indent + "  ";                                                        \
+                                                                                                   \
+    prefix = new_indent + "what()= ";                                                              \
+    output = #TYPE "{\n" + prefix                                                                  \
+             + export_var(exception.what(), new_indent, prefix.length(), next_depth, false) + "\n" \
+             + indent + "}";                                                                       \
+                                                                                                   \
+    return output;                                                                                 \
+  }
 
 namespace cpp_dump {
 
@@ -32,6 +52,17 @@ namespace _detail {
 
 template <typename T>
 std::string export_var(const T &, const std::string &, size_t, size_t, bool);
+
+_p_CPP_DUMP_DEFINE_EXPORT_EXCEPTION(std::logic_error);
+_p_CPP_DUMP_DEFINE_EXPORT_EXCEPTION(std::domain_error);
+_p_CPP_DUMP_DEFINE_EXPORT_EXCEPTION(std::invalid_argument);
+_p_CPP_DUMP_DEFINE_EXPORT_EXCEPTION(std::length_error);
+_p_CPP_DUMP_DEFINE_EXPORT_EXCEPTION(std::out_of_range);
+_p_CPP_DUMP_DEFINE_EXPORT_EXCEPTION(std::runtime_error);
+_p_CPP_DUMP_DEFINE_EXPORT_EXCEPTION(std::range_error);
+_p_CPP_DUMP_DEFINE_EXPORT_EXCEPTION(std::overflow_error);
+_p_CPP_DUMP_DEFINE_EXPORT_EXCEPTION(std::underflow_error);
+_p_CPP_DUMP_DEFINE_EXPORT_EXCEPTION(std::exception);
 
 template <typename T>
 inline auto export_exception(
