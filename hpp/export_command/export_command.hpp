280d80a7aa55cd0adb35f20041cb7553648f0e15
philip82148 <92205270+philip82148@users.noreply.github.com>
2023-10-14 05:29:12 +0900

    add int_style()

--- hpp/export_command/export_command.hpp
+++ hpp/export_command/export_command.hpp
@@ -23,7 +23,7 @@ namespace _detail {
 
 const std::shared_ptr<
     std::function<std::optional<std::size_t>(std::size_t, const std::function<std::size_t()> &)>>
-    _default_skip_size_func(
+    _default_skip_size_func_ptr(
         std::make_shared<std::function<
             std::optional<std::size_t>(std::size_t, const std::function<std::size_t()> &)>>(
             [](std::size_t index, std::function<std::size_t()> get_size) {
@@ -36,112 +36,174 @@ template <typename T>
 struct value_with_command;
 
 struct export_command {
-  friend export_command _map_key(const export_command &command) {
-    export_command new_command;
-    new_command.map_key_skip_size_func = command.skip_size_func;
-    new_command.map_key_child          = command.child;
-    return new_command;
+  friend export_command _map_key(export_command &&command) {
+    export_command new_command(nullptr);
+    new_command.map_key_child = std::make_shared<export_command>(std::move(command));
+    return std::move(new_command);
   }
 
-  friend export_command _map_value(const export_command &command) {
-    export_command new_command;
-    new_command.map_value_skip_size_func = command.skip_size_func;
-    new_command.map_value_child          = command.child;
-    return new_command;
+  friend export_command _map_value(export_command &&command) {
+    export_command new_command(nullptr);
+    new_command.map_value_child = std::make_shared<export_command>(std::move(command));
+    return std::move(new_command);
   }
 
-  friend export_command _map_key_and_value(const export_command &key, const export_command &value) {
-    export_command new_command;
-    new_command.map_key_skip_size_func   = key.skip_size_func;
-    new_command.map_key_child            = key.child;
-    new_command.map_value_skip_size_func = value.skip_size_func;
-    new_command.map_value_child          = value.child;
-    return new_command;
+  friend export_command _map_key_and_value(export_command &&key, export_command &&value) {
+    export_command new_command(nullptr);
+    new_command.map_key_child   = std::make_shared<export_command>(std::move(key));
+    new_command.map_value_child = std::make_shared<export_command>(std::move(value));
+    return std::move(new_command);
   }
 
  public:
+  export_command(const std::pair<int, int> &int_style)
+      : int_style(int_style), skip_size_func_ptr(nullptr) {}
+
   export_command(
       std::function<std::optional<std::size_t>(std::size_t, const std::function<std::size_t()> &)>
           &&skip_size_func
   )
-      : skip_size_func(
-          std::make_shared<std::function<
-              std::optional<std::size_t>(std::size_t, const std::function<std::size_t()> &)>>(
-              std::move(skip_size_func)
-          )
-      ) {}
-  export_command(
-      const std::shared_ptr<std::function<
-          std::optional<std::size_t>(std::size_t, const std::function<std::size_t()> &)>>
-          &skip_size_func,
-      const std::shared_ptr<export_command> &child
-  )
-      : skip_size_func(skip_size_func), child(child) {}
+      : int_style(0, 0),
+        skip_size_func_ptr(
+            skip_size_func ? std::make_shared<std::function<
+                    std::optional<std::size_t>(std::size_t, const std::function<std::size_t()> &)>>(
+                std::move(skip_size_func)
+            )
+                           : nullptr
+        ) {}
+
   export_command(const export_command &)            = default;
   export_command &operator=(const export_command &) = default;
-  export_command() : skip_size_func(_default_skip_size_func) {}
+  export_command(export_command &&)                 = default;
+  export_command &operator=(export_command &&)      = default;
 
-  template <typename T>
-  skip_container<T> get_skip_container(const T &container) const {
-    return skip_container<T>(container, *skip_size_func);
+  export_command() : int_style(0, 0), skip_size_func_ptr(_default_skip_size_func_ptr) {}
+
+  export_command &&operator<<(export_command &&command) && {
+    *this << std::move(command);
+    return std::move(*this);
+  }
+
+  export_command &operator<<(export_command &&command) & {
+    // export_command has either {map_key_child || map_value_child}, skip_size_func_ptr
+    // int_style.
+    // in case of {map_key_child || map_value_child}
+    if (command.map_key_child || command.map_value_child) {
+      // jump to the node whose child has no skip_size_func_ptr.
+      if (child && child->skip_size_func_ptr) {
+        *child << std::move(command);
+        return *this;
+      }
+
+      // assign to map_key_child and map_value_child of the node, which has the last
+      // skip_size_func_ptr of the chain. and they inherit int_style from the node.
+      auto &int_style_to_inherit = child && child->child && child->child->int_style.first
+                                       ? child->child->int_style
+                                       : int_style;
+      if (command.map_key_child) {
+        map_key_child = std::move(command.map_key_child);
+        if (!map_key_child->int_style.first) map_key_child->apply_int_style(int_style_to_inherit);
+      }
+      if (command.map_value_child) {
+        map_value_child = std::move(command.map_value_child);
+        if (!map_value_child->int_style.first)
+          map_value_child->apply_int_style(int_style_to_inherit);
+      }
+      return *this;
+    }
+
+    // in case of skip_size_func_ptr
+    if (command.skip_size_func_ptr) {
+      // jump to the node that has no skip_size_func_ptr
+      if (skip_size_func_ptr && child) {
+        *child << std::move(command);
+        return *this;
+      }
+
+      // assign
+      if (!skip_size_func_ptr) {
+        skip_size_func_ptr = command.skip_size_func_ptr;
+      } else {
+        child            = std::make_shared<export_command>(std::move(command));
+        child->int_style = int_style;
+      }
+      return *this;
+    }
+
+    // in case of int_style
+    // jump to the node that has no skip_size_func_ptr
+    // or create that has no skip_size_func_ptr
+    if (skip_size_func_ptr) {
+      if (!child) {
+        child            = std::make_shared<export_command>(nullptr);
+        child->int_style = int_style;
+      }
+      *child << std::move(command);
+      return *this;
+    }
+
+    // this node is the first node that will have skip_size_func_ptr next.
+    // assign int_style to the child of this node, so that int_style applies to the element of the
+    // container.
+    if (!child) child = std::make_shared<export_command>(nullptr);
+    child->int_style = command.int_style;
+
+    return *this;
   }
 
   export_command next() const {
     if (child) return *child;
 
-    return export_command();
+    return export_command(int_style);
   }
 
   export_command next_for_map_key() const {
-    if (map_key_child) return *map_key_child;
+    if (skip_size_func_ptr && map_key_child) return *map_key_child;
 
     return next();
   }
 
   export_command next_for_map_value() const {
-    if (map_value_child) return *map_value_child;
+    if (skip_size_func_ptr && map_value_child) return *map_value_child;
 
     return next();
   }
 
   template <typename T>
-  value_with_command<T> operator<<(const T &value) {
-    return value_with_command<T>(value, *this);
+  value_with_command<T> operator<<(const T &value) && {
+    return value_with_command<T>(value, std::move(*this));
   }
 
-  export_command &operator<<(const export_command &command) {
-    if (child) {
-      *child << command;
-    } else {
-      child.reset(new export_command(command.skip_size_func, command.child));
+  template <typename T>
+  skip_container<T> get_skip_container(const T &container) const {
+    if (skip_size_func_ptr) return skip_container<T>(container, *skip_size_func_ptr);
+    return skip_container<T>(container, *_default_skip_size_func_ptr);
+  }
 
-      if (command.map_key_skip_size_func)
-        map_key_child.reset(
-            new export_command(command.map_key_skip_size_func, command.map_key_child)
-        );
-
-      if (command.map_value_skip_size_func)
-        map_value_child.reset(
-            new export_command(command.map_value_skip_size_func, command.map_value_child)
-        );
-    }
-
-    return *this;
+  std::pair<int, int> get_int_style() const {
+    auto [base, chunk] = int_style;
+    return {base >= 2 && base <= 16 ? base : 0, chunk >= 0 ? chunk : 0};
   }
 
  private:
+  std::pair<int, int> int_style;
   std::shared_ptr<
       std::function<std::optional<std::size_t>(std::size_t, const std::function<std::size_t()> &)>>
-      skip_size_func;
+      skip_size_func_ptr;
   std::shared_ptr<export_command> child;
-  std::shared_ptr<
-      std::function<std::optional<std::size_t>(std::size_t, const std::function<std::size_t()> &)>>
-      map_key_skip_size_func;
   std::shared_ptr<export_command> map_key_child;
-  std::shared_ptr<
-      std::function<std::optional<std::size_t>(std::size_t, const std::function<std::size_t()> &)>>
-      map_value_skip_size_func;
   std::shared_ptr<export_command> map_value_child;
+
+  void apply_int_style(const std::pair<int, int> &int_style) {
+    if (!int_style.first) return;
+
+    this->int_style = int_style;
+
+    if (child && !child->int_style.first) child->apply_int_style(int_style);
+    if (map_key_child && !map_key_child->int_style.first) map_key_child->apply_int_style(int_style);
+    if (map_value_child && !map_value_child->int_style.first)
+      map_value_child->apply_int_style(int_style);
+  }
 };
 
 template <typename T>
@@ -150,8 +212,8 @@ struct value_with_command {
   const T &value;
   const export_command command;
 
-  value_with_command(const T &value, const export_command &command)
-      : value(value), command(command) {}
+  value_with_command(const T &value, export_command &&command)
+      : value(value), command(std::move(command)) {}
   value_with_command() = delete;
 };
 
@@ -164,6 +226,10 @@ template <typename T>
 inline constexpr bool is_value_with_command = _is_value_with_command<_remove_cref<T>>;
 
 }  // namespace _detail
+
+inline auto int_style(int base = 10, int chunk = 4) {
+  return _detail::export_command({base, chunk});
+}
 
 inline auto keep_front(std::size_t iteration_count = max_iteration_count) {
   return _detail::export_command([=](std::size_t index,
@@ -193,12 +259,12 @@ inline auto keep_middle(std::size_t iteration_count = max_iteration_count) {
   });
 }
 
-auto map_key(const _detail::export_command &command) { return _map_key(command); }
+auto map_key(_detail::export_command &&command) { return _map_key(std::move(command)); }
 
-auto map_value(const _detail::export_command &command) { return _map_value(command); }
+auto map_value(_detail::export_command &&command) { return _map_value(std::move(command)); }
 
-auto map_key_and_value(const _detail::export_command &key, const _detail::export_command &value) {
-  return _map_key_and_value(key, value);
+auto map_key_and_value(_detail::export_command &&key, _detail::export_command &&value) {
+  return _map_key_and_value(std::move(key), std::move(value));
 }
 
 }  // namespace cpp_dump
--- hpp/export_var/export_arithmetic.hpp
+++ hpp/export_var/export_arithmetic.hpp
@@ -29,9 +29,40 @@ export_arithmetic(char value, const std::string &, std::size_t, std::size_t, boo
 }
 
 template <typename T>
+inline auto export_arithmetic(
+    const T &value,
+    const std::string &,
+    std::size_t,
+    std::size_t,
+    bool,
+    const export_command &command
+) -> std::enable_if_t<is_arithmetic<T> && std::is_integral_v<T>, std::string> {
+  auto int_style = command.get_int_style();
+  T base = static_cast<T>(int_style.first), chunk = static_cast<T>(int_style.second);
+  if (base == 0 || value < 0) return es::number(std::to_string(value));
+
+  std::string output;
+  T tmp = value;
+  int i = 0;
+  for (; i == 0 || tmp; ++i) {
+    T r = tmp % base;
+    if (chunk > 0 && i > 0 && i % chunk == 0) output = " " + output;
+    if (r <= 9) {
+      output = static_cast<char>(r + '0') + output;
+    } else {
+      output = static_cast<char>(r - 10 + 'a') + output;
+    }
+    tmp /= base;
+  }
+  for (; chunk > 0 && i % chunk; ++i) output = "0" + output;
+
+  return es::number(output) + es::op(" _" + std::to_string(base));
+}
+
+template <typename T>
 inline auto
 export_arithmetic(const T &value, const std::string &, std::size_t, std::size_t, bool, const export_command &)
-    -> std::enable_if_t<is_arithmetic<T>, std::string> {
+    -> std::enable_if_t<is_arithmetic<T> && !std::is_integral_v<T>, std::string> {
   return es::number(std::to_string(value));
 }
 
