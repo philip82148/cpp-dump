8b2b4c7f9550397c93ab1b2d478fb402b30ad685
philip82148 <92205270+philip82148@users.noreply.github.com>
2023-10-14 12:43:46 +0900

    update dump test

--- test/dump_test.cpp
+++ test/dump_test.cpp
@@ -237,6 +237,15 @@ int main(int argc, char *argv[]) {
   // extra
   CPP_DUMP(cpp_dump::es_style_t::no_es, cpp_dump::es_style_t::by_syntax, cpp_dump::es_value);
 
+  // int_style()
+  CPP_DUMP(
+      int_style(16) << 10000u,
+      int_style(16) << 10000l,
+      int_style(16) << 10000ul,
+      int_style(16) << 10000ll,
+      int_style(16) << 10000ull
+  );
+
   // show_*()
   array<int, 50> array50;
   map<int, int> map50;
@@ -290,18 +299,19 @@ int main(int argc, char *argv[]) {
   CPP_DUMP(show_front(2) << map_key_and_value(show_back(2), show_front(2)) << multimap2);
 
   CPP_DUMP(
-      int_style(12, 3) << show_front(2) << int_style(2, 0)
-                       << map_key_and_value(int_style(16, 3) << show_back(2), show_front(2))
-                       << multimap2
+      int_style(12, 3, 3) << show_front(2) << int_style(2, 0, 0)
+                          << map_key_and_value(int_style(16, 3, 3) << show_back(2), show_front(2))
+                          << multimap2
   );
   CPP_DUMP(
-      int_style(12, 3) << int_style(8, 3)
-                       << map_key_and_value(int_style(16, 3) << show_back(2), show_front(2))
-                       << multimap2
+      int_style(12, 3, 3) << int_style(8, 3, 3)
+                          << map_key_and_value(int_style(16, 3, 3) << show_back(2), show_front(2))
+                          << multimap2
   );
   CPP_DUMP(
       show_front(1) << map_value(
-          int_style(8, 3) << show_middle(1) << int_style(16, 3) << show_middle(2) << int_style(5, 3)
+          int_style(8, 3, 3) << show_middle(1) << int_style(16, 3, 3) << show_middle(2)
+                             << int_style(5, 3, 3)
       ) << multimap2
   );
   rep(i, 17) CPP_DUMP(i, int_style(i) << 0x10000);
