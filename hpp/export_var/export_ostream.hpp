21892cbc03e24f0b0288e3dac93c846c9e5ab69b
philip82148 <92205270+philip82148@users.noreply.github.com>
2023-11-06 22:00:14 +0900

    add export_ostream()

---
+++ hpp/export_var/export_ostream.hpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2023 Ryota Sasaki.
+ *
+ * This source code is licensed under the MIT license found in the LICENSE file in the root
+ * directory of this source tree.
+ */
+
+#pragma once
+
+#include <sstream>
+#include <string>
+#include <type_traits>
+
+#include "../export_command/export_command.hpp"
+#include "../type_check.hpp"
+
+namespace cpp_dump {
+
+namespace _detail {
+
+template <typename T>
+inline auto
+export_ostream(const T &value, const std::string &, std::size_t, std::size_t, bool, const export_command &)
+    -> std::enable_if_t<is_ostream<T>, std::string> {
+  std::ostringstream ss;
+  ss << value;
+
+  return ss.str();
+}
+
+}  // namespace _detail
+
+}  // namespace cpp_dump
