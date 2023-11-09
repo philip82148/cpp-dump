4d4f8cb4d8415caa181cdc1a4f89afd250d57c10
philip82148 <92205270+philip82148@users.noreply.github.com>
2023-11-09 12:44:30 +0900

    update test

--- test/dump_variable_test.cpp
+++ test/dump_variable_test.cpp
@@ -11,33 +11,41 @@ using namespace std;
 using namespace cpp_dump;
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) return 1;
-  int es_index = stoi(argv[1]);
+  if (argc != 3) return 1;
+  bool compiler_dependent = static_cast<bool>(stoi(argv[1]));
+  bool es_index = static_cast<bool>(stoi(argv[2]));
 
   CPP_DUMP_SET_OPTION(
       es_style, (array{cpp_dump::es_style_t::no_es, cpp_dump::es_style_t::by_syntax}[es_index])
   );
 
-  // pointer
-  const void *void_ptr = (void *)0x7ffd06586204;
-  cpp_dump(void_ptr);
+  if (compiler_dependent) {
+    class original_error : public logic_error {
+      using logic_error::logic_error;
+    } original_error1("This is an original error.");
+    cpp_dump(original_error1);
+  } else {
+    // pointer
+    const void *void_ptr = (void *)0x7ffd06586204;
+    cpp_dump(void_ptr);
 
-  // unordered
-  cpp_dump((unordered_map<int, int>{{4, 6}, {2, 6}, {4, 3}}));
+    // unordered
+    cpp_dump((unordered_map<int, int>{{4, 6}, {2, 6}, {4, 3}}));
 
-  unordered_multimap<char, int> unordered_multimap1;
-  unordered_multimap1.emplace('c', 30);
-  unordered_multimap1.emplace('a', 10);
-  unordered_multimap1.emplace('b', 20);
-  unordered_multimap1.emplace('a', 40);
-  cpp_dump(unordered_multimap1);
+    unordered_multimap<char, int> unordered_multimap1;
+    unordered_multimap1.emplace('c', 30);
+    unordered_multimap1.emplace('a', 10);
+    unordered_multimap1.emplace('b', 20);
+    unordered_multimap1.emplace('a', 40);
+    cpp_dump(unordered_multimap1);
 
-  cpp_dump((unordered_set{3, 1, 4, 1, 5}));
+    cpp_dump((unordered_set{3, 1, 4, 1, 5}));
 
-  unordered_multiset<int> unordered_multiset1;
-  unordered_multiset1.insert(3);
-  unordered_multiset1.insert(1);
-  unordered_multiset1.insert(4);
-  unordered_multiset1.insert(1);
-  cpp_dump(unordered_multiset1);
+    unordered_multiset<int> unordered_multiset1;
+    unordered_multiset1.insert(3);
+    unordered_multiset1.insert(1);
+    unordered_multiset1.insert(4);
+    unordered_multiset1.insert(1);
+    cpp_dump(unordered_multiset1);
+  }
 }
