5c534c6d83e375450b94a854387fd05d5245d7d4
philip82148 <92205270+philip82148@users.noreply.github.com>
2023-11-11 02:36:55 +0900

    update README.md

--- readme/formatting-with-manipulators.cpp
+++ readme/formatting-with-manipulators.cpp
@@ -22,10 +22,17 @@ int main() {
 
   std::clog << std::endl;
 
+  // useful-manipulators.png
+  cpp_dump(cp::int_style10(2) << cp::show_back(10) << cp::show_both_ends(10) << some_huge_vector);
+
+  std::clog << std::endl;
+
+  // omitting-a-vector.png
   cpp_dump(cp::show_back(10) << cp::show_both_ends(10) << some_huge_vector);
 
   std::clog << std::endl;
 
+  // int-style.png
   cpp_dump(cp::int_style(2, 8, 2) << cp::show_front(5) << cp::show_front(5) << some_huge_vector);
 
   std::clog << std::endl;
