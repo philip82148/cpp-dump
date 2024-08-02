f69b95a42caa5960304f290f0481be6113477761
philip82148 <92205270+philip82148@users.noreply.github.com>
2024-08-02 20:30:25 +0900

    dump type_info::hash_code() in export_type_info()

--- hpp/export_var/export_other/export_type_info.hpp
+++ hpp/export_var/export_other/export_type_info.hpp
@@ -23,9 +23,14 @@ namespace cpp_dump {
 namespace _detail {
 
 template <typename T>
-inline auto
-export_type_info(const T &type_info, const std::string &indent, std::size_t last_line_length, std::size_t current_depth, bool fail_on_newline, const export_command &)
-    -> std::enable_if_t<is_type_info<T>, std::string> {
+inline auto export_type_info(
+    const T &type_info,
+    const std::string &indent,
+    std::size_t last_line_length,
+    std::size_t current_depth,
+    bool fail_on_newline,
+    const export_command &command
+) -> std::enable_if_t<is_type_info<T>, std::string> {
   std::string class_name =
       es::class_name(std::is_same_v<T, std::type_info> ? "std::type_info" : "std::type_index");
 
@@ -36,16 +41,14 @@ export_type_info(const T &type_info, const std::string &indent, std::size_t last
   std::string name = type_info.name();
 #endif
 
-  _p_CPP_DUMP_DEFINE_EXPORT_OBJECT_COMMON1_1;
-  [[maybe_unused]] auto _dummy_1 = next_depth;
-  [[maybe_unused]] auto _dummy_2 = is_first = true;
-
-  _p_CPP_DUMP_DEFINE_EXPORT_OBJECT_COMMON1_3;
+  _p_CPP_DUMP_DEFINE_EXPORT_OBJECT_COMMON1;
 
   if (shift_indent) output += "\n" + new_indent;
-
   output += es::class_member("name()") + es::op("= ");
   output += es::class_op(R"(")") + es::type_name(name) + es::class_op(R"(")");
+
+  is_first = false;
+  append_output(es::class_member("has_code()"), type_info.hash_code());
 
   _p_CPP_DUMP_DEFINE_EXPORT_OBJECT_COMMON2;
 }
