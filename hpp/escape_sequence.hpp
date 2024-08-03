3f0b4919a379dbd78973afab9466a08b6707e689
philip82148 <92205270+philip82148@users.noreply.github.com>
2024-08-03 12:31:10 +0900

    improve es::apply()

--- hpp/escape_sequence.hpp
+++ hpp/escape_sequence.hpp
@@ -29,7 +29,9 @@ inline std::string reset() { return use_es() ? std::string(_reset_es) : std::str
 
 inline std::string apply(std::string_view es, std::string_view s) {
   if (use_es()) {
-    return std::move(std::string(es).append(s).append(_reset_es));
+    std::string retval(es);
+    retval.append(s).append(_reset_es);
+    return retval;
   } else {
     return std::string(s);
   }
