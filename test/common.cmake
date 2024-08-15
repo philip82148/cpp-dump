7015d7ff1059a4714fedead8557492a9a883a354
philip82148 <92205270+philip82148@users.noreply.github.com>
2024-08-15 14:37:35 +0900

    fix test in windows

--- test/common.cmake
+++ test/common.cmake
@@ -2,12 +2,18 @@ string(ASCII 27 esc0x1b)
 
 function(diff_and_message log_file txt_file error_message)
     if(WIN32)
-        execute_process(COMMAND diff "(cat ${log_file})" "(cat ${txt_file})" RESULT_VARIABLE not_successful OUTPUT_VARIABLE output)
+        execute_process(
+            COMMAND "${CMAKE_COMMAND}" -E compare_files "${log_file}" "${txt_file}" RESULT_VARIABLE not_successful
+        )
     else()
         execute_process(COMMAND diff -u "${log_file}" "${txt_file}" RESULT_VARIABLE not_successful OUTPUT_VARIABLE output)
     endif()
 
     if(not_successful)
+        if(WIN32)
+            file(READ "${log_file}" output)
+        endif()
+
         message(SEND_ERROR "${error_message}\n${output}")
     endif()
 endfunction()
