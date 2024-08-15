630cff3862f8427711213fc9f49ca4983d2d2839
philip82148 <92205270+philip82148@users.noreply.github.com>
2024-08-15 20:21:24 +0900

    rename macro name

--- hpp/export_var/export_object_generic.hpp
+++ hpp/export_var/export_object_generic.hpp
@@ -15,37 +15,37 @@
 #include "../type_check.hpp"
 #include "./export_object_common.hpp"
 
-#define _p_CPP_DUMP_EXPAND_FOR_DANGEROUS_EXPORT_OBJECT(member)  value.member
-#define _p_CPP_DUMP_EXPAND_FOR_DANGEROUS_EXPORT_OBJECT2(member) append_output(#member, value.member)
+#define _p_CPP_DUMP_EXPAND_FOR_EXPORT_OBJECT_GENERIC(member)  value.member
+#define _p_CPP_DUMP_EXPAND_FOR_EXPORT_OBJECT_GENERIC2(member) append_output(#member, value.member)
 
 /**
  * Make export_var() support every type that has the specified members.
  * Member functions to be displayed must be const.
  * Compile errors in this macro, such as ambiguous function calls, are never reported due to SFINAE.
  */
-#define CPP_DUMP_DEFINE_EXPORT_OBJECT_GENERIC(...)                                                                   \
-  namespace cpp_dump {                                                                                               \
-                                                                                                                     \
-  namespace _detail {                                                                                                \
-                                                                                                                     \
-  template <typename T>                                                                                              \
-  inline auto export_object_generic(                                                                                 \
-      const T &value,                                                                                                \
-      const std::string &indent,                                                                                     \
-      std::size_t last_line_length,                                                                                  \
-      std::size_t current_depth,                                                                                     \
-      bool fail_on_newline,                                                                                          \
-      const export_command &command                                                                                  \
-  ) -> decltype(_p_CPP_DUMP_EXPAND_VA(_p_CPP_DUMP_EXPAND_FOR_DANGEROUS_EXPORT_OBJECT, __VA_ARGS__), std::string()) { \
-    std::string class_name = es::class_name(get_typename<T>());                                                      \
-                                                                                                                     \
-    _p_CPP_DUMP_DEFINE_EXPORT_OBJECT_COMMON1;                                                                        \
-                                                                                                                     \
-    _p_CPP_DUMP_EXPAND_VA(_p_CPP_DUMP_EXPAND_FOR_DANGEROUS_EXPORT_OBJECT2, __VA_ARGS__);                             \
-                                                                                                                     \
-    _p_CPP_DUMP_DEFINE_EXPORT_OBJECT_COMMON2;                                                                        \
-  }                                                                                                                  \
-                                                                                                                     \
-  } /* namespace _detail */                                                                                          \
-                                                                                                                     \
+#define CPP_DUMP_DEFINE_EXPORT_OBJECT_GENERIC(...)                                                                 \
+  namespace cpp_dump {                                                                                             \
+                                                                                                                   \
+  namespace _detail {                                                                                              \
+                                                                                                                   \
+  template <typename T>                                                                                            \
+  inline auto export_object_generic(                                                                               \
+      const T &value,                                                                                              \
+      const std::string &indent,                                                                                   \
+      std::size_t last_line_length,                                                                                \
+      std::size_t current_depth,                                                                                   \
+      bool fail_on_newline,                                                                                        \
+      const export_command &command                                                                                \
+  ) -> decltype(_p_CPP_DUMP_EXPAND_VA(_p_CPP_DUMP_EXPAND_FOR_EXPORT_OBJECT_GENERIC, __VA_ARGS__), std::string()) { \
+    std::string class_name = es::class_name(get_typename<T>());                                                    \
+                                                                                                                   \
+    _p_CPP_DUMP_DEFINE_EXPORT_OBJECT_COMMON1;                                                                      \
+                                                                                                                   \
+    _p_CPP_DUMP_EXPAND_VA(_p_CPP_DUMP_EXPAND_FOR_EXPORT_OBJECT_GENERIC2, __VA_ARGS__);                             \
+                                                                                                                   \
+    _p_CPP_DUMP_DEFINE_EXPORT_OBJECT_COMMON2;                                                                      \
+  }                                                                                                                \
+                                                                                                                   \
+  } /* namespace _detail */                                                                                        \
+                                                                                                                   \
   }  // namespace cpp_dump
