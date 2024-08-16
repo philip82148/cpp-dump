085a3d9228ee47df1ed41f2e8805dcf0540c16d9
philip82148 <92205270+philip82148@users.noreply.github.com>
2024-08-16 21:40:16 +0900

    add dump.hpp for compatiblity

---
+++ dump.hpp
@@ -0,0 +1,4 @@
+#pragma message(                                                                                   \
+    "WARNING: The 'dump.hpp' header is deprecated. Include the 'cpp-dump.hpp' header instead."     \
+)
+#include "./cpp-dump.hpp"
