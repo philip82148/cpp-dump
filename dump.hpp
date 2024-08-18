51deae5bfc9dc13135c4ade73b622ef6e1807177
philip82148 <92205270+philip82148@users.noreply.github.com>
2024-08-18 16:40:48 +0900

    add comment about dump.hpp

--- dump.hpp
+++ dump.hpp
@@ -1,3 +1,4 @@
+// See https://github.com/philip82148/cpp-dump/pull/100 for details of this file.
 #pragma once
 #pragma message(                                                                                   \
     "WARNING: The 'dump.hpp' header is deprecated. Include the 'cpp-dump.hpp' header instead."     \
