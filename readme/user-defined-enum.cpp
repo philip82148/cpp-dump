74c489c944a513472c87e01da13cd7530fff1dd4
philip82148 <92205270+philip82148@users.noreply.github.com>
2024-09-25 21:13:13 +0900

    improve user-defined-enum.cpp

--- readme/user-defined-enum.cpp
+++ readme/user-defined-enum.cpp
@@ -2,13 +2,13 @@
 
 #include "../cpp-dump.hpp"
 
-enum class enum_A { a, b, c };
-CPP_DUMP_DEFINE_EXPORT_ENUM(enum_A, enum_A::a, enum_A::b, enum_A::c);
+enum class enum_A { member_a, member_b, member_c };
+CPP_DUMP_DEFINE_EXPORT_ENUM(enum_A, enum_A::member_a, enum_A::member_b, enum_A::member_c);
 
 int main() {
   std::clog << std::endl;
 
-  enum_A my_enum_A = enum_A::c;
+  enum_A my_enum_A = enum_A::member_c;
   cpp_dump(my_enum_A);
 
   std::clog << std::endl;
